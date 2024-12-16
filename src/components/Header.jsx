1116f0efdafc1536e57878e16398cbdc3c90d819
Adeyemi Olaoye <yhemi06@gmail.com>
2024-12-16 19:55:23 +0100

    updated react version and added more content to header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,47 @@
 import React from "react";
-
+import { TbBusinessplan } from "react-icons/tb";
+import { Link } from "react-router-dom";
+// import { truncate, useGlobalState } from "../store";
 const Header = () => {
-  return <div>Header</div>;
+  // const [connectedAccount] = useGlobalState("connectedAccount");
+  return (
+    <div
+      className="flex justify-between items-center
+        p-5 bg-white shadow-lg fixed top-0 left-0 right-0"
+    >
+      <Link
+        to="/"
+        className="flex justify-start items-center
+      text-xl text-black space-x-1"
+      >
+        <span>Genesis</span>
+        <TbBusinessplan />
+      </Link>
+
+      <div className="flex space-x-2 justify-center">
+        {/* {connectedAccount ? ( */}
+        {/* <button
+            type="button"
+            className="inline-block px-6 py-2.5 bg-green-600
+            text-white font-medium text-xs leading-tight uppercase
+            rounded-full shadow-md hover:bg-green-700"
+          >
+            {truncate(connectedAccount, 4, 4, 11)}
+          </button> */}
+        {/* ) : (*/}
+        <button
+          type="button"
+          className="inline-block px-6 py-2.5 bg-green-600
+            text-white font-medium text-xs leading-tight uppercase
+            rounded-full shadow-md hover:bg-green-700"
+          // onClick={connectWallet}
+        >
+          Connect Wallet
+        </button>
+       {/*  )} */}
+      </div>
+    </div>
+  );
 };
 
 export default Header;
